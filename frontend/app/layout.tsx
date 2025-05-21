7135d91ed7d27b95ee989d7b88bc7f46a1c5884d
Lucas Bonny <lucas@gunthercloud.com.br>
2025-05-21 18:20:00 -0300

    Limpeza front

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,7 +5,6 @@ import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
-import '../styles/demo/Demos.scss';
 
 interface RootLayoutProps {
     children: React.ReactNode;
@@ -13,9 +12,9 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="pt-BR" suppressHydrationWarning>
             <head>
-                <link id="theme-css" href={`/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
+                <link id="theme-css" href={`/themes/lara-dark-blue/theme.css`} rel="stylesheet"></link>
             </head>
             <body>
                 <PrimeReactProvider>
