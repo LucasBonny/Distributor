d90f71cb57bafde598317790b4b7291265595596
Lucas Bonny <lucas@gunthercloud.com.br>
2025-06-11 15:22:48 -0300

    Product Repository OK

--- backend/src/test/java/br/com/gunthercloud/distributor/resources/Factory.java
+++ backend/src/test/java/br/com/gunthercloud/distributor/resources/Factory.java
@@ -1,11 +1,11 @@
 package br.com.gunthercloud.distributor.resources;
 
+import java.util.UUID;
+
 import br.com.gunthercloud.distributor.entities.Product;
 import br.com.gunthercloud.distributor.entities.Supplier;
 import br.com.gunthercloud.distributor.entities.dto.ProductDTO;
 import br.com.gunthercloud.distributor.entities.dto.SupplierDTO;
-
-import java.util.UUID;
 
 public class Factory {
 
@@ -18,7 +18,9 @@ public class Factory {
     }
 
     public static Product createProduct() {
-        return new Product(22L, 129483712L, "Chocolate", 12.0, 1, "https://google.com/", createSupplier());
+    	Supplier sup = createSupplier();
+    	sup.setId(UUID.fromString("550e8400-e29b-41d4-a716-446655440001"));
+        return new Product(22L, 129483712L, "Chocolate", 12.0, 1, "https://google.com/", sup);
     }
 
     public static ProductDTO createProductDTO() {
